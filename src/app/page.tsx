6f902981f481d2fbd0db58e54aabd2160b30daad
Abhinav priyadarshi <abhiz01p@gmail.com>
2026-02-16 23:27:25 +0530

    Setup project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+        <h1 className="text-[198px] flex h-screen bg-cyan-500 font-bold underline">
+            Darpan
+        </h1>
+    </main>
+  );
+}
